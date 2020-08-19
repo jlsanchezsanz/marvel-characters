f2bdf147d9a6ec742c329d8e99b452995413d440
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2020-08-19 20:20:53 +0200

    feat(character-details): fetch character details

--- src/components/CharacterDetailsContainer/CharacterDetailsContainer.js
+++ src/components/CharacterDetailsContainer/CharacterDetailsContainer.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
+import { fetchCharacterDetails } from '../../state/actions/character-details.actions';
 import CharacterDetails from './CharacterDetails';
 
-function CharacterDetailsContainer({ character, isLoading }) {
+function CharacterDetailsContainer({ character, dispatch }) {
+  let { id } = useParams();
+
+  useEffect(() => {
+    if (!character) {
+      dispatch(fetchCharacterDetails(id));
+    }
+  }, [dispatch, id, character]);
+
   return !character ? (
     <p>Loading...</p>
   ) : (
@@ -11,11 +21,21 @@ function CharacterDetailsContainer({ character, isLoading }) {
   );
 }
 
+function getCharacterFromDetails(state) {
+  return state.characterDetailsReducer.characterDetails;
+}
+
+function getCharacterFromList(state, id) {
+  return state.charactersReducer.characters.find(
+    (character) => character.id.toString() === id
+  );
+}
+
 const mapStateToProps = (state, ownProps) => ({
-  character: state.charactersReducer.characters.find(
-    (character) => character.id.toString() === ownProps.match.params.id
-  ),
-  isLoading: state.charactersReducer.isLoading
+  character:
+    getCharacterFromDetails(state) ||
+    getCharacterFromList(state, ownProps.match.params.id),
+  isLoading: state.characterDetailsReducer.isLoading
 });
 
 export default connect(mapStateToProps)(CharacterDetailsContainer);
--- src/components/CharacterDetailsContainer/__tests__/CharacterDetailsContainer.test.js
+++ src/components/CharacterDetailsContainer/__tests__/CharacterDetailsContainer.test.js
@@ -3,11 +3,23 @@ import { mount } from 'enzyme';
 
 import CharacterDetailsContainer from '../CharacterDetailsContainer';
 import { mockStore } from '../../../state/__mocks__/store';
+import { fetchCharacterDetails } from '../../../state/actions/character-details.actions';
 
 jest.mock('../CharacterDetails', () => () => <></>);
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({ id: 1011334 })
+}));
+jest.mock('../../../state/actions/character-details.actions', () => ({
+  ...jest.requireActual('../../../state/actions/character-details.actions'),
+  fetchCharacterDetails: jest.fn()
+}));
+
+let store;
 
 const setUpMount = (initialState, match) => {
-  const store = mockStore(initialState);
+  store = mockStore(initialState);
+  store.dispatch = jest.fn();
   const component = mount(
     <CharacterDetailsContainer store={store} match={match} />
   );
@@ -16,10 +28,17 @@ const setUpMount = (initialState, match) => {
 
 describe('CharacterDetailsContainer', () => {
   let component;
+  const id = 1011334;
 
   it('should display loading message while no character', () => {
     component = setUpMount(
-      { charactersReducer: { characters: [], isLoading: true } },
+      {
+        charactersReducer: { characters: [] },
+        characterDetailsReducer: {
+          characterDetails: undefined,
+          isLoading: true
+        }
+      },
       { params: {} }
     );
     expect(component).toMatchSnapshot();
@@ -27,9 +46,35 @@ describe('CharacterDetailsContainer', () => {
 
   it('should display character details', () => {
     component = setUpMount(
-      { charactersReducer: { characters: [{ id: '1011334' }] } },
-      { params: { id: '1011334' } }
+      {
+        charactersReducer: { characters: [] },
+        characterDetailsReducer: { characterDetails: [{ id }] }
+      },
+      { params: { id: id.toString() } }
     );
     expect(component).toMatchSnapshot();
+  });
+
+  it('should fetch character details with the id in the url', () => {
+    component = setUpMount(
+      {
+        charactersReducer: { characters: [] },
+        characterDetailsReducer: { characterDetails: undefined }
+      },
+      { params: { id: id.toString() } }
+    );
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(store.dispatch).toHaveBeenCalledWith(fetchCharacterDetails(id));
+  });
+
+  it('should not fetch character details if already in the store', () => {
+    component = setUpMount(
+      {
+        charactersReducer: { characters: [{ id }] },
+        characterDetailsReducer: { characterDetails: undefined }
+      },
+      { params: { id: id.toString() } }
+    );
+    expect(store.dispatch).not.toHaveBeenCalled();
   });
 });
