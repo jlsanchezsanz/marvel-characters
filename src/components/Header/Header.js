dbc3016ad44b2830377fd42bad58e9e0a97b23bf
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2020-08-22 21:22:06 +0200

    feat(Header): navigate to home on logo click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.scss';
 
 export default function Header() {
   return (
-    <nav class='navbar navbar-light bg-light header'>
-      <a class='navbar-brand' href='#'>
+    <nav className='navbar navbar-light bg-light header'>
+      <Link to='/'>
         <img
           src={require('../../assets/marvel_logo.svg')}
           alt='Marvel'
           height='40'
         />
-      </a>
+      </Link>
     </nav>
   );
 }
