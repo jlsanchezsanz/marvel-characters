a20082589e051ca1babd2e8194aface3a1e057e9
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2020-08-24 18:37:12 +0200

    chore(*): setup heroku

---
+++ build/precache-manifest.f520025b064afa04471728eaa1e90552.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ddee30e205d99b70e718786a09a41f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e51f18fb8cdfd324ebb",
+    "url": "/static/css/2.8c03b716.chunk.css"
+  },
+  {
+    "revision": "501188351a99d2db6256",
+    "url": "/static/css/3.add0420b.chunk.css"
+  },
+  {
+    "revision": "5fb3d947d2c47a7ca092",
+    "url": "/static/css/4.a030bdc3.chunk.css"
+  },
+  {
+    "revision": "45eb84dacfe3e6325537",
+    "url": "/static/css/main.ebdde2c0.chunk.css"
+  },
+  {
+    "revision": "9e51f18fb8cdfd324ebb",
+    "url": "/static/js/2.cf33754e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cf33754e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501188351a99d2db6256",
+    "url": "/static/js/3.f56e3258.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/3.f56e3258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb3d947d2c47a7ca092",
+    "url": "/static/js/4.7e69d7d5.chunk.js"
+  },
+  {
+    "revision": "45eb84dacfe3e6325537",
+    "url": "/static/js/main.e4c69680.chunk.js"
+  },
+  {
+    "revision": "f5f285389e947a0152a8",
+    "url": "/static/js/runtime-main.5287c481.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e475ecfedfaa57b5ca186143161c8ae1",
+    "url": "/static/media/marvel_logo.e475ecfe.svg"
+  }
+]);
