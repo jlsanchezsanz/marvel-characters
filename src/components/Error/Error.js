03bb8322038d22a73258eef63058f0f1f624fecb
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2020-08-25 18:48:10 +0200

    styles(Error): fix padding

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './Error.scss';
+
 export default function Error({ message }) {
   return (
-    <div className='alert alert-danger' role='alert'>
-      {message}
+    <div className='error'>
+      <div className='alert alert-danger' role='alert'>
+        {message}
+      </div>
     </div>
   );
 }
